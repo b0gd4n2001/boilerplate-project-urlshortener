185a5ef9a187a51fbe7584a2bd8ea10f29136816
b0gd4n2001 <lulelarub@gmail.com>
2024-04-17 13:02:16 +0300

    nothing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('node:dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +24,13 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.post('/api/shorturl', bodyParser.urlencoded({extended: false}), function(req, res) {
+  
+  dns.lookup(req.body.url, function (err, address, family) {
+    console.log(err);
+    console.log(address);
+    console.log(family);
+  });
+  res.json({ original_url: '', short_url: '' });
+})
